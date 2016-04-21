5781c27ae47e02b4f40ceed41e37b87427b8c568
Florian Heinze <florian.heinze@sandstorm-media.de>
2016-04-21 11:56:51 +0200

    TASK: added new logo + cleanup

--- logos/svg/svg2png.sh
+++ logos/svg/svg2png.sh
@@ -1,4 +1,9 @@
 rm -rf ../png/*
+rm -rf ../../avatars/*
+
+convert  -density 1000 -resize x250 neos_N_primary.svg ../../avatars/avatar_250.png
+convert  -density 1000 -resize x500 neos_N_primary.svg ../../avatars/avatar_500.png
+convert  -density 1000 -resize x1080 neos_N_primary.svg ../../avatars/avatar_1080.png
 
 for i in *.svg; do
 	convert -background none -density 1000 -resize x2160 $i ../png/`basename $i .svg`_2160.png
