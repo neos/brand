f3bd35c69c7edecca75cbd9248026db8cc665ea5
Tyll Weiß <info@craft-interactive.de>
2016-07-31 12:53:04 +0200

    FEATURE: Add a file for colors and font-settings extracted from the brand guide
    This file can be installed and used by packages which contain stylings. Ideally these variables aren't duplicated across all packages, instead they should be fetched from one single source. This commit tries to solve this problem of cluttering brand settings

---
+++ index.js
@@ -0,0 +1,33 @@
+//
+// This file can be installed and imported across packages to have one
+// single source of truth for colors and general `styleguide` styles
+// of the Neos brand.
+//
+module.exports = {
+	colors: {
+		primaryViolet: '#26224C',
+		primaryBlue: '#00ADEE',
+		contrastDarkest: '#141414',
+		contrastDarker: '#222',
+		contrastDark: '#3f3f3f',
+		contrastNeutral: '#323232',
+		contrastBright: '#999',
+		contrastBrighter: '#adadad',
+		contrastBrightest: '#FFF',
+		success: '#00a338',
+		warn: '#ff8700',
+		error: '#ff460d'
+	},
+	fonts: {
+		headings: {
+			family: 'Work Sans',
+			style: 'Light',
+			_cssWeight: '300'
+		},
+		copy: {
+			family: 'Work Sans',
+			style: 'Regular',
+			_cssWeight: '400'
+		}
+	}
+};
